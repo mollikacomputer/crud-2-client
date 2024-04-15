f76efe3baa0f3cb56fbd44ea82ee9f64593ae686
MollikaComputer Provider <mollikacomputer3@gmail.com>
2024-04-15 21:21:46 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+
+  return (
+    <>
+      <h2 className='text-5xl'> Home Page</h2>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/AddCoffee/AddCoffee.jsx
@@ -0,0 +1,86 @@
+
+const AddCoffee = () => {
+
+  const handleAddCoffee = event =>{
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    const phone = event.target.phone.value;
+    const newCoffee = {name, email, password, phone};
+    
+    console.log(newCoffee);
+
+    fetch('http://localhost:5000/coffee',{
+      method:"POST",
+      headers:{
+        'content-type':'application/json'
+      },
+      body:JSON.stringify(newCoffee),
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      if(data.insertedId){
+        alert("New Coffee Added Successfully")
+      }
+    })
+
+  }
+  return (
+    <div className="container mx-auto">
+      <form onSubmit={handleAddCoffee} className="">
+        <label className="form-control w-full max-w-xs">
+          <div className="label">
+            <span className="label-text">Coffee Name</span>
+          </div>
+          <input
+            type="text"
+            name="name"
+            placeholder="Coffy Name"
+            className="input input-bordered w-full max-w-xs"
+          />
+        </label>
+        <label className="form-control w-full max-w-xs">
+          <div className="label">
+            <span className="label-text">Quantity</span>
+          </div>
+          <input
+            type="number"
+            name="phone"
+            placeholder="Coffy Quantity"
+            className="input input-bordered w-full max-w-xs"
+          />
+        </label>
+        <label className="form-control w-full max-w-xs">
+          <div className="label">
+            <span className="label-text">Email</span>
+          </div>
+          <input
+            type="email"
+            name="email"
+            placeholder="email"
+            className="input input-bordered w-full max-w-xs"
+          />
+        </label>
+        <label className="form-control w-full max-w-xs">
+          <div className="label">
+            <span className="label-text">Password</span>
+          </div>
+          <input
+            type="tepassword"
+            name="password"
+            placeholder="Password"
+            className="input input-bordered w-full max-w-xs"
+          />
+        
+        <input className="btn btn-primary w-full" type="submit" value="Add A Coffee" />
+        </label>
+        
+
+      </form>
+    </div>
+  );
+};
+
+export default AddCoffee;
